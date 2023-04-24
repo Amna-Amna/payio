103b35028aa274a4a832fe882b20b0766d2562b4
Amna-Amna <mailtoamna4@gmail.com>
2023-04-24 19:41:59 +0500

    feat: Add totalDepositsUSD function to convert euros to USD

--- script.js
+++ script.js
@@ -105,3 +105,14 @@ const calcDisplaySummary = function (movements) {
 };
 
 calcDisplaySummary(account1.movements);
+
+const eurToUsd = 1.1;
+const totalDepositsUSD = movements
+  .filter((mov) => mov > 0)
+  .map((mov, i, arr) => {
+    return mov * eurToUsd;
+  })
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
+
+console.log(movements);
