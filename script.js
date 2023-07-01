d37311ce48e83a2a22c48d6c6709b0ddd6fcace1
Amna-Amna <mailtoamna4@gmail.com>
2023-07-01 15:40:47 +0500

    add few changes in displayMovements function

--- script.js
+++ script.js
@@ -95,13 +95,19 @@ const displayMovements = function (movements, sort = false) {
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? "deposit" : "withdrawal";
 
+    const date = new Date(acc.movementsDates[i]);
+    const displayDate = formatMovementDate(date, acc.locale);
+
+    const formattedMov = formatCur(mov, acc.locale, acc.currency);
+
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
-      </div>
+    <div class="movements__date">${displayDate}</div>
+    <div class="movements__value">${formattedMov}</div>
+  </div>
     `;
 
     containerMovements.insertAdjacentHTML("afterbegin", html);
